7ddaa213fcbb13725a272c57d38eb85418a6c538
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-02 10:09:39 -0400

    Agregado ejemplo 1 de Observables

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -10,6 +11,23 @@ export class LayoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    /* Ejemplo 1: Observables. Unicast por defecto.
+       Cada suscribible ve su propia ejecución del observable */
+    
+    const randomNumberObservable = new Observable((observer) => {
+      observer.next(Math.random());
+    });
 
+    const subscriber1 = randomNumberObservable.subscribe(number => {
+      console.log(`Subscriber1 received ${number}`)
+    });
+
+    const subscriber2 = randomNumberObservable.subscribe((number) => {
+      console.log(`Subscriber2 received ${number}`);
+    });
+
+    const subscriber3 = randomNumberObservable.subscribe((number) => {
+      console.log(`Subscriber3 received ${number}`);
+    });
+  }
 }
